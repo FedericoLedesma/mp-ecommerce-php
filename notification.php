dd327416e67899419c0055b52b6b81df4d9180c7
Federico Ledesma <federledesma10@gmail.com>
2020-08-21 17:32:41 -0300

    Correcciones

--- notification.php
+++ notification.php
@@ -4,31 +4,17 @@
 
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-    $json_event = file_get_contents('php://input', true);
-    $json_event_decode = json_decode($json_event);
-    $logFile = fopen("log-mp-json.txt", 'a') or die("Error creando archivo");
-    fwrite($logFile, print_r($json_event_decode, true));
-    fclose($logFile);
+    $entityBody = file_get_contents('php://input');
+    
+    if(!isset($entityBody)){
+        $fichero = './webhook.json';
 
-    $logFile = fopen("log-mp-json-dos.txt", 'a') or die("Error creando archivo");
-    fwrite($logFile, print_r($json_event, true));
-    fclose($logFile);
-
-    if ($_GET["topic"] == "payment") {
-        
-        $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
-        $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
-        
-        $logFile = fopen("log-mp-dos.txt", 'a') or die("Error creando archivo");
-        fwrite($logFile, print_r($payment, true));
-        fclose($logFile);
-
-        $logFile = fopen("log-mp-tres.txt", 'a') or die("Error creando archivo");
-        fwrite($logFile, print_r($merchant_order, true));
-        fclose($logFile);
-    } 
-
-    ob_clean();
-    header("HTTP/1.1 200 OK");
-
+        $actual = file_get_contents($fichero);
+    
+        $actual .= $entityBody;
+    
+        file_put_contents($fichero, $actual);
+    } else{
+        header('Location: https://federledesma-mp-ecommerce-php.herokuapp.com/');
+    }
 ?>
--- pago-aceptado.php
+++ pago-aceptado.php
@@ -10,7 +10,7 @@
     
     <meta name="viewport" content="width=1024">
     <title>Tienda e-commerce</title>
-    <script src="https://www.mercadopago.com/v2/security.js" view="home"></script>
+    <script src="https://www.mercadopago.com/v2/security.js" view="pago-aceptado"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="format-detection" content="telephone=no">
 
